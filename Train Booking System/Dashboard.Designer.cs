e1368d8d2d6aa08a664c16195d45ced6b05c2003
tungminhv github <tungminh4399@gmail.com>
2024-05-18 22:03:50 +0700

    add things

---
+++ Train Booking System/Dashboard.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Train_Booking_System
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(115, 158);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(136, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(110, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WHO R U";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 306);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Dashboard";
+            this.Text = "UIAdmin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Train Booking System/Dashboard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Train_Booking_System
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Normal user");
+            comboBox1.Items.Add("Addmin");
+
+            comboBox1.SelectedIndex = 0;
+
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the selected item text
+            string selectedItem = comboBox1.SelectedItem.ToString();
+
+            // Display the selected item in the label
+            if (selectedItem == "Normal user")
+            {
+                LoginForm user = new LoginForm();
+                user.Show();
+            }
+            else
+            {
+                //Application.Run(new AdminLogin());
+                AdminLogin adminlogin = new AdminLogin();
+                adminlogin.Show();
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
