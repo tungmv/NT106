f1cb4fad1e4ac70531d489c6f6d18f51570f0837
evgafanboi <22521115@gm.uit.edu.vn>
2024-06-04 21:08:44 +0700

    Database included haizz

--- Server API/Server API/obj/Debug/net8.0/Server API.AssemblyInfo.cs
+++ Server API/Server API/obj/Debug/net8.0/Server API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d154d0d722e07de57eaa61837ba50b7bcd39fc10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
